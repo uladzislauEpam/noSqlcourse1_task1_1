cfe6853d5f15d3984a94e0e93228a29d36c685b0
Uladzislau_Maksimuk <uladzislau_maksimuk@epam.com>
2024-10-21 10:05:41 +0300

    fix1

--- src/main/java/epam/uladzislau/model/Subtask.java
+++ src/main/java/epam/uladzislau/model/Subtask.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "subtasks")
+@Document(collection = "tasks")
 public class Subtask {
     @Id
     private String id;
